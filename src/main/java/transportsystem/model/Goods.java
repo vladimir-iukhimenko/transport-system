62fc563941c29b4d40f38bb89b4470075aca3cb2
Vladimir Iukhimenko <vlad.imenko@yandex.ru>
2019-11-16 17:26:09 +0300

    Corrected Goods and Nomenclature

--- src/main/java/transportsystem/model/Goods.java
+++ src/main/java/transportsystem/model/Goods.java
@@ -1,16 +1,33 @@
 package transportsystem.model;
 
 public class Goods {
-	private Nomenclature nomenclature;
-	private int amount;
-	private double length;
-	private double height;
-	private double weight;
+	private Integer id;
+	private Integer nomenclatureid;
+	private Integer amount;
+	private Double length;
+	private Double height;
+	private Double weight;
 	
-	public Goods(Nomenclature nomenclature, int amount, double length, double height, double weight) {
-		this.nomenclature = nomenclature;
+	public Goods(Integer nomenclatureid, Integer amount, Double length, Double height, Double weight) {
+		this.nomenclatureid = nomenclatureid;
 		this.amount = amount;
 		this.length = length;
-		
+		this.height = height;
+        this.weight = weight;
 	}
+
+    public Goods() {this(0,0,0.0,0.0,0.0);}
+
+    public Integer getId() {return this.id;}
+    public Integer getNomenclatureid() {return this.nomenclatureid;}
+    public Integer getAmount() {return this.amount;}
+    public Double getLength() {return this.length;}
+    public Double getHeight() {return this.height;}
+    public Double getWeight() {return this.weight;}
+    public void setId(Integer id) {this.id = id;}
+    public void setNomenclatureid(Integer nomenclatureid) {this.nomenclatureid = nomenclatureid;}
+    public void setAmount(Integer amount) {this.amount = amount;}
+    public void setLength(Double length) {this.length = length;}
+    public void setHeight(Double height) {this.height = height;}
+    public void setWeight(Double weight) {this.weight = weight;}
 }
--- src/main/java/transportsystem/model/Nomenclature.java
+++ src/main/java/transportsystem/model/Nomenclature.java
@@ -1,18 +1,33 @@
 package transportsystem.model;
 
+import java.time.LocalDateTime;
+
 public class Nomenclature {
-	private double length;
-	private double width;
-	private double height;
-	private double weight;
+	private Integer id;
+	private Double length;
+	private Double width;
+	private Double height;
+	private Double weight;
 	private StringBuilder comment;
-	public Nomenclature(double length, double width, double height, double weight) {
+	public Nomenclature(Double length, Double width, Double height, Double weight) {
 		this.length = length;
 		this.width = width;
 		this.height = height;
 		this.weight = weight;
 	}
+
+	public Nomenclature() {this(0.0,0.0,0.0,0.0);}
+
+	public Integer getId() {return this.id;}
+	public Double getLength() {return this.length;}
+	public Double getWidth() {return this.width;}
+	public Double getHeight() {return this.height;}
+	public Double getWeight() {return this.weight;}
 	public String getComment() {return this.comment.toString();}
-	//TODO: implement function
-	public boolean addComment() {return false;}
+    public void setId(Integer id) {this.id = id;}
+    public void setLength(Double length) {this.length = length;}
+	public void setWidth(Double width) {this.width = width;}
+    public void setHeight(Double height) {this.height = height;}
+    public void setWeight(Double weight) {this.weight = weight;}
+    public void setComment(String comment) {this.comment.append(LocalDateTime.now().toString() + comment + "\r\n");}
 }
