da6d3efc19dad50ce4f9aa9f119870a5d05c02f3
Vladimir Iukhimenko <vlad.imenko@yandex.ru>
2019-11-27 15:14:49 +0300

    Added Employee

---
+++ src/main/java/transportsystem/controller/EmployeeController.java
@@ -0,0 +1,79 @@
+package transportsystem.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import transportsystem.model.Employee;
+import transportsystem.service.EmployeeService;
+
+import java.util.List;
+
+/**
+ * Created by DZCKJS0 on 27.11.2019.
+ */
+@Controller
+public class EmployeeController {
+    private EmployeeService employeeService;
+
+    @Autowired
+    public void setEmployeeService(EmployeeService employeeService) {this.employeeService = employeeService;}
+
+    @RequestMapping(value = "/employees", method = RequestMethod.GET)
+    public ModelAndView listEmployees() {
+        List<Employee> listEmployees = employeeService.getAllEmployees();
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("employees");
+        modelAndView.addObject("employees",listEmployees);
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/employees/add", method = RequestMethod.GET)
+    public ModelAndView addEmployee() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("editoremployees");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/employees/add", method = RequestMethod.POST)
+    public ModelAndView addEmployee(@ModelAttribute("employee") Employee employee)
+    {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("redirect:/employees");
+        employeeService.add(employee);
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/employees/edit/{id}", method = RequestMethod.GET)
+    public ModelAndView editEmployee(@PathVariable("id") int id)
+    {
+        Employee employee = employeeService.getEmployeeById(id);
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("editoremployees");
+        modelAndView.addObject("employee", employee);
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/employees/edit", method = RequestMethod.POST)
+    public ModelAndView editEmployee(@ModelAttribute("employee") Employee employee)
+    {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("redirect:/employees");
+        employeeService.edit(employee);
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/employees/delete/{id}", method = RequestMethod.GET)
+    public ModelAndView deleteEmployee(@PathVariable("id") int id)
+    {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("redirect:/employees");
+        Employee employee = employeeService.getEmployeeById(id);
+        employeeService.delete(employee);
+        return modelAndView;
+    }
+}
--- src/main/java/transportsystem/model/Document.java
+++ src/main/java/transportsystem/model/Document.java
@@ -1,5 +1,8 @@
 package transportsystem.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.time.LocalDate;
 
@@ -9,34 +12,34 @@ public abstract class Document {
 	@Id
 	@Column(name="id")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Getter
+	@Setter
     protected Integer id;
 
     @Column(name="documenttype")
+	@Getter
+	@Setter
 	protected String documenttype;
 
     @Column(name="series")
+	@Getter
+	@Setter
 	protected Integer series;
 
     @Column(name="number")
+	@Getter
+	@Setter
 	protected Integer number;
 
     @Column(name="issuedby")
+	@Getter
+	@Setter
 	protected String issuedby;
 
     @Column(name="issueddate")
+	@Getter
 	protected LocalDate issueddate;
-	
-	public Integer getId() {return this.id;}
-    public String getDocumenttype() {return this.documenttype;}
-	public Integer getSeries() {return this.series;}
-	public Integer getNumber() {return this.number;}
-	public String getIssuedby() {return this.issuedby;}
-	public LocalDate getIssueddate() {return this.issueddate;}
-	public void setDocumenttype(String documenttype) {this.documenttype = documenttype;}
-    public void setSeries(Integer series) {this.series = series;}
-    public void setNumber(Integer number) {this.number = number;}
-    public void setIssuedby(String issuedby) {this.issuedby = issuedby;}
+
     public void setIssueddate(String issueddate) {this.issueddate = LocalDate.parse(issueddate);}
-    public void setId(Integer id) {this.id = id;}
 	
 }
