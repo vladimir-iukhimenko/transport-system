fb8f5a0aa7bf38ccf17c4e4279d7bd4178aa835d
Vladimir <Vladimir Iukhimenko>
2020-05-23 22:36:11 +0300

    Changed the project structure

--- src/main/frontend/src/service/RestAPIService.js
+++ src/main/frontend/src/service/RestAPIService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import authHeader from "./AuthHeader";
 
-const API_URL = "http://localhost:8080/api";
+const API_URL = 'https://trans-system.herokuapp.com/api'
 
 class RestAPIService{
     readAll(path) {
