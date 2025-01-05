1bb919aa5e113fd7cbbd7f7af775443ecb4e02b6
Vladimir Iukhimenko <vlad.imenko@gmail.com>
2025-01-05 18:00:53 +0100

    use flyway, refactor configuration

--- src/main/java/com/transportsystem/backend/BackendApplication.java
+++ src/main/java/com/transportsystem/backend/BackendApplication.java
@@ -25,84 +25,14 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import javax.servlet.http.HttpServletResponse;
-import javax.sql.DataSource;
 import java.util.Collections;
-import java.util.Properties;
 
 @EnableSwagger2
-@SpringBootApplication(exclude = {
-		DataSourceAutoConfiguration.class,
-		DataSourceTransactionManagerAutoConfiguration.class,
-		HibernateJpaAutoConfiguration.class,
-		})
-@EntityScan("com.transportsystem.backend.model")
+@SpringBootApplication
 public class BackendApplication {
-	private Environment environment;
-
-	@Autowired
-	public void setEnvironment(Environment environment) {
-		this.environment = environment;
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
-	}
-
-	//@Bean
-	public DataSource dataSource() {
-		BasicDataSource dataSource = new BasicDataSource();
-		dataSource.setDriverClassName(environment.getProperty("spring.datasource.driver-class-name"));
-		dataSource.setUrl(environment.getProperty("spring.datasource.url"));
-		dataSource.setUsername(environment.getProperty("spring.datasource.username"));
-		dataSource.setPassword(environment.getProperty("spring.datasource.password"));
-		return dataSource;
-	}
-
-	@Bean
-	public LocalSessionFactoryBean sessionFactory() {
-		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-
-		sessionFactory.setPackagesToScan(new String[] { "" });
-		sessionFactory.setDataSource(dataSource());
-		sessionFactory.setHibernateProperties(hibernateProperties());
-		return sessionFactory;
-	}
-
-	@Bean
-	public HikariDataSource createHikariConfig() {
-		HikariConfig hikariConfig = new HikariConfig();
-		hikariConfig.setDriverClassName(environment.getProperty("spring.datasource.driver-class-name"));
-		hikariConfig.setJdbcUrl(environment.getProperty("spring.datasource.url"));
-		hikariConfig.setUsername(environment.getProperty("spring.datasource.username"));
-		hikariConfig.setPassword(environment.getProperty("spring.datasource.password"));
-		return new HikariDataSource(hikariConfig);
-	}
-
-	private Properties hibernateProperties() {
-		Properties properties = new Properties();
-		properties.put("hibernate.dialect", environment.getRequiredProperty("spring.jpa.properties.hibernate.dialect"));
-		properties.put("hibernate.naming.physical-strategy", environment.getRequiredProperty("spring.jpa.hibernate.naming.physical-strategy"));
-		properties.put("hibernate.naming.strategy", environment.getRequiredProperty("spring.jpa.hibernate.naming.physical-strategy"));
-		properties.put("hibernate.show_sql", environment.getRequiredProperty("spring.jpa.show-sql"));
-		properties.put("hibernate.ddl-auto", environment.getRequiredProperty("spring.jpa.hibernate.ddl-auto"));
-		properties.put("hibernate.id.new_generator_mappings", environment.getRequiredProperty("spring.jpa.hibernate.use-new-id-generator-mappings"));
-		properties.put("hibernate.search.default.directory_provider", environment.getRequiredProperty("spring.jpa.properties.hibernate.search.default.directory_provider"));
-		properties.put("current_session_context_class", environment.getRequiredProperty("spring.jpa.properties.hibernate.current_session_context_class"));
-		properties.put("hibernate.search.default.indexBase", environment.getRequiredProperty("spring.jpa.properties.hibernate.search.default.indexBase"));
-		properties.put("hibernate.format_sql", environment.getRequiredProperty("spring.jpa.properties.hibernate.format_sql"));
-		properties.put("hibernate.use_sql_comments", environment.getRequiredProperty("spring.jpa.properties.hibernate.use_sql_comments"));
-		properties.put("hibernate.max_fetch_depth", environment.getRequiredProperty("spring.jpa.properties.hibernate.max_fetch_depth"));
-		//properties.put("hibernate.search.default.locking_strategy", environment.getRequiredProperty("hibernate.search.default.locking_strategy"));
-		//properties.put("hibernate.search.lucene_version",environment.getRequiredProperty("hibernate.search.lucene_version"));
-		return properties;
-	}
-
-	@Bean
-	public HibernateTransactionManager transactionManager() {
-		HibernateTransactionManager transactionManager = new HibernateTransactionManager();
-		transactionManager.setSessionFactory(sessionFactory().getObject());
-		return transactionManager;
 	}
 
 	@Bean
