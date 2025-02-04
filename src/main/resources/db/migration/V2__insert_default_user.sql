b61d084051e29b3fa4380113bac1f00cf8ff6b2f
natalia.iukhimenko <natalia.yukhimenko@gmail.com>
2025-02-04 21:51:55 +0100

    Updated sql script for inserting admin user

--- src/main/resources/db/migration/V2__insert_default_user.sql
+++ src/main/resources/db/migration/V2__insert_default_user.sql
@@ -1,8 +1,22 @@
-insert into roles (id, name)
-values (1, 'ROLE_ADMIN');
+DO $$
+DECLARE
+admin_role_name VARCHAR(255);
+    admin_username VARCHAR(255);
+    admin_password VARCHAR(255);
+    role_id INTEGER;
+    user_id INTEGER;
+BEGIN
+    admin_role_name := 'ROLE_ADMIN';
+    admin_username := 'ts_admin';
+    admin_password := 'ts_admin';
+insert into roles (name)
+values (admin_role_name);
 
-insert into users (id, enabled, password, username)
-values (1, true, 'ts_admin', 'ts_admin');
+insert into users (enabled, password, username)
+values (true, admin_password, admin_username);
 
-insert into user_roles (user_id, role_id)
-values (1, 1);
+select users.id into user_id from users where username = admin_username;
+select roles.id into role_id from roles where name = admin_role_name;
+insert into user_roles(user_id, role_id) values (user_id, role_id);
+END;
+$$
