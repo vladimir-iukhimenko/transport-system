98d4f0925e8b0de3346c99b511a22f360e8824cf
natalia.iukhimenko <natalia.yukhimenko@gmail.com>
2025-02-28 13:57:54 +0100

    Added custom attribute 'test-id' for elements side menu

--- frontend/src/components/SideMenu.vue
+++ frontend/src/components/SideMenu.vue
@@ -2,7 +2,7 @@
     <div role="tablist">
         <b-card no-body class="mb-1">
             <b-card-header header-tag="header" class="p-1" role="tab">
-                <b-button block href="#" v-b-toggle.accordion-1 variant="info">Транспортные средства</b-button>
+                <b-button block href="#" v-b-toggle.accordion-1 test-id="vehicles_menu" variant="info">Транспортные средства</b-button>
             </b-card-header>
             <b-collapse id="accordion-1" visible accordion="my-accordion" role="tabpanel">
                 <b-card-body>
@@ -15,7 +15,7 @@
         </b-card>
         <b-card no-body class="mb-1">
             <b-card-header header-tag="header" class="p-1" role="tab">
-                <b-button block href="#" v-b-toggle.accordion-2 variant="info">Заказы</b-button>
+                <b-button block href="#" v-b-toggle.accordion-2 test-id="orders_menu" variant="info">Заказы</b-button>
             </b-card-header>
             <b-collapse id="accordion-2" accordion="my-accordion" role="tabpanel">
                 <b-card-body>
@@ -26,7 +26,7 @@
         </b-card>
         <b-card no-body class="mb-1">
             <b-card-header header-tag="header" class="p-1" role="tab">
-                <b-button block href="#" v-b-toggle.accordion-3 variant="info">Грузы</b-button>
+                <b-button block href="#" v-b-toggle.accordion-3 test-id="cargoes_menu" variant="info">Грузы</b-button>
             </b-card-header>
             <b-collapse id="accordion-3" accordion="my-accordion" role="tabpanel">
                 <b-card-body>
@@ -36,7 +36,7 @@
         </b-card>
         <b-card no-body class="mb-1">
             <b-card-header header-tag="header" class="p-1" role="tab">
-                <b-button block href="#" v-b-toggle.accordion-4 variant="info">Сотрудники</b-button>
+                <b-button block href="#" v-b-toggle.accordion-4 test-id="employees_menu" variant="info">Сотрудники</b-button>
             </b-card-header>
             <b-collapse id="accordion-4" accordion="my-accordion" role="tabpanel">
                 <b-card-body>
